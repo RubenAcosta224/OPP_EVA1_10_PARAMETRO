22c677807f4fbd790df25940cf335e78c2b73e9b
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-02-22 22:26:36 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETRO/src/eva1_10_parametro/EVA1_10_PARAMETRO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_parametro;
+
+/**
+ *
+ * @author hp
+ */
+public class EVA1_10_PARAMETRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        nPrintLn("si",5);
+    }
+    public static void nPrintLn (String message,int n){
+        for (int i = 0; i <= n ; i++ ) {
+            System.out.println(message);
+        }
+        
+    }
+}
